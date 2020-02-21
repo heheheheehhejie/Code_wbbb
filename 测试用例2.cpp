759d304cd5a96b0a7fa5b2b17f834b2c4f036ce0
heheheheehhejie <46087562+heheheheehhejie@users.noreply.github.com>
2020-02-21 22:07:39 +0800

    Add files via upload

---
+++ 测试用例2.cpp
@@ -0,0 +1,23 @@
+#include<cstdlib>
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include"code_waibibabu.h" 
+using namespace std;
+int main()
+{
+	char Input_file[200000];
+	memset(Input_file,0,sizeof(Input_file));
+	scanf("%s",Input_file);
+	
+	freopen(Input_file,"r",stdin);
+	freopen("out_put.txt","w",stdout);
+	
+	char Input_text[200000]; 
+	gets(Input_text);
+	printf("%s",encode_wbbb(Input_text));
+	fclose(stdin);
+	fclose(stdout);
+	
+	return 0;
+} 
