124d09fc01b917331870bf0697c01ff7e7daf61a
heheheheehhejie <46087562+heheheheehhejie@users.noreply.github.com>
2020-02-21 22:16:16 +0800

    Update 测试用例2.cpp

--- 测试用例2.cpp
+++ 测试用例2.cpp
@@ -6,14 +6,15 @@
 using namespace std;
 int main()
 {
-	char Input_file[200000];
+	char Input_file[200000];//文件路径最大长度
 	memset(Input_file,0,sizeof(Input_file));
 	scanf("%s",Input_file);
 	
 	freopen(Input_file,"r",stdin);
 	freopen("out_put.txt","w",stdout);
 	
-	char Input_text[200000]; 
+	char Input_text[20000]; //文本最大长度
+	memset(Input_text,0,sizeof(Input_text));
 	gets(Input_text);
 	printf("%s",encode_wbbb(Input_text));
 	fclose(stdin);
